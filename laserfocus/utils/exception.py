42620b300d9dd97177446336dc92fa053f338769
Andres Aguilar <aguilarcarboni@gmail.com>
2025-03-09 18:06:05 -0600

    Fix exception to use Flask object not app's response object

--- laserfocus/utils/exception.py
+++ laserfocus/utils/exception.py
@@ -1,12 +1,17 @@
 from .logger import logger
-from flask import jsonify
+from flask import Response
+import json
 
 def handle_exception(func):
     def wrapper(*args, **kwargs):
         try:
-            response = func(*args, **kwargs)
-            return jsonify(response), 200
+            data = func(*args, **kwargs)
+            if not isinstance(data, dict) or not isinstance(data, list) or not isinstance(data, tuple):
+                data = {'data': data}
+            
+            return Response(json.dumps(data), status=200)
         except Exception as e:
             logger.error(f"Error in {func.__name__}: {e}")
-            return jsonify({'error': str(e)}), 500
+            json_data = json.dumps({'error': str(e)})
+            return Response(json_data, status=500)
     return wrapper
